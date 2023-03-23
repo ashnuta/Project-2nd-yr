9b1d31e0db151986b5ad68a6bb0748b830f8e530
ashnuta <128633983+ashnuta@users.noreply.github.com>
2023-03-23 15:17:08 +0530

    Add files via upload

---
+++ Renderer.java
@@ -0,0 +1,17 @@
+package flappyBird;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	
+	protected void paintComponent(Graphics g) 
+	{
+		super.paintComponent(g);
+		
+		FlappyBird.flappyBird.repaint(g);
+	}
+}
